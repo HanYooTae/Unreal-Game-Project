22ee8de9e95b99a2f6ec89a64c18d2d3cd7bed82
HanYooTae <han931120@gmail.com>
2023-10-25 00:04:21 +0900

    Make Player Actions DataAssets

---
+++ Source/Main/CActionData.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CActionData.generated.h"
+
+UCLASS()
+class MAIN_API UCActionData : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
