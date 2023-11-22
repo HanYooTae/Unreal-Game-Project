619b3edf408c7b048f25ca31aa16ce5e3310ca79
HanYooTae <han931120@gmail.com>
2023-11-23 00:21:50 +0900

    attach Bullet

--- Source/Main/Actions/CDoAction_Sniper.cpp
+++ Source/Main/Actions/CDoAction_Sniper.cpp
@@ -4,6 +4,7 @@
 #include "CharacterComponents/CStateComponent.h"
 #include "CharacterComponents/CStatusComponent.h"
 #include "GameFramework/Character.h"
+#include "CBullet.h"
 #include "Global.h"
 
 void ACDoAction_Sniper::BeginPlay()
@@ -33,6 +34,37 @@ void ACDoAction_Sniper::DoAction()
 
 void ACDoAction_Sniper::Begin_DoAction()
 {
+	// Spawn Projectile
+	CheckNull(Datas[0].ProjectileClass);
+
+	// 카메라의 위치, 회전
+	FVector location;
+	FRotator rotation;
+	OwnerCharacter->GetController()->GetPlayerViewPoint(location, rotation);
+
+	FVector handSocketLocation = OwnerCharacter->GetMesh()->GetSocketLocation("hand_r");
+	FVector cameraDirection = rotation.Vector();
+
+	location += cameraDirection * ((handSocketLocation - location) | cameraDirection);
+
+	FTransform transform = Datas[0].EffectTransform;
+	transform.AddToTranslation(location);
+
+	transform.SetRotation(FQuat(OwnerCharacter->GetControlRotation()));		// != OwnerCharacter->GetActorForwardVector()->Rotation()
+
+	ACBullet* bullet = GetWorld()->SpawnActorDeferred<ACBullet>
+		(
+			Datas[0].ProjectileClass,
+			transform,
+			OwnerCharacter,
+			OwnerCharacter,
+			ESpawnActorCollisionHandlingMethod::AlwaysSpawn
+			);
+
+	// 충돌처리
+	
+
+	bullet->FinishSpawning(transform);
 }
 
 void ACDoAction_Sniper::End_DoAction()
